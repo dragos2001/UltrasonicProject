9cc00c40fd41c4d9e608e2625d0bb41e3b37a9ce
FIXED-TERM Bratfalean Dragos (XC-AS/EPS2-RO) <fixed-term.Dragos.Bratfalean@ro.bosch.com>
2025-09-16 16:36:53 +0300

    Solved USB connection between MS and WSL. I basically attached once the device in application mode, after which I entered the device in BOOTSEL, and then I had to attach the BOOTSEL device because its VID:PID was changed, after that i could upload the code using sudo picotool load, followed by rebooting back to application mode and then I detached the device from MS

--- project/build_load_wsl.sh
+++ project/build_load_wsl.sh
@@ -1,41 +1,99 @@
-current_dir=$(pwd)
-echo "$current_dir"
-echo "This Script is responsible for creating project's build-system, building main.c and uploading to rp2350"
-build_dir="$current_dir"/"build"
-echo "$build_dir"
-if [ -d  "$build_dir" ]; then
-	echo "build dir exists."
-	cd  "$build_dir"
-	if [ "$(ls -A $build_dir)" ]; then
-		echo "build from cache..."
-		cmake . || goto error
-	else
-		echo "build from scratch..."
-		cmake .. || goto error
-	fi
+#!/bin/bash
+
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m' 
+NC='\033[0m' 
+
+#Step 1:Build and Make the project
+
+#Check for build dir
+build_dir="build"
+if [ -d $build_dir ];then
+  echo -e "$PURPLE Directory exists "
+else
+  echo -e "$PURPLE Directory doesn't exist "
+  mkdir $build_dir
+fi
+
+cd $build_dir
+#Create build filesystem
+echo -e "$CYAN Creating the file system... "
+cmake ./
+a=$?
+if [ $a -ne 0 ]; then
+  echo -e "$RED Creating buildsystem failed! " 
+  rm -rf ./*
+  cmake ..
+  a=$?
+  if [$a -ne 0 ]; then
+    echo -e "$RED Creating buildsystem failed again! "
+  else
+    echo -e "$CYAN Successfully created the build filesystem!"
+  fi
+else
+  echo -e "$CYAN Successfully created the build filesystem!"
+
+fi
+
+#Navigate to app
+cd "app"
+echo -e "$CYAN Building the firmware... "
+
+#Build
+make -j8
+if [ $? -ne 0 ]; then
+  echo -e "$RED Building firmware failed! "
+  exit 1
+fi
+
+#Step 2: Attach USB device (application mode)
+echo -e " $CYAN Attaching USB device... "
+BUSID=1-4
+#List USB devices
+cmd.exe /c usbipd list > log.txt
+#Look for 2e8a:0009 VID:PID and chain the corresponding line to fragment using awk and return the first field BUSID
+BUSID=$(grep '2e8a:0009' log.txt | awk '{print $1}')
+echo "Busid:"
+echo $BUSID
+#Attach to the BUSID
+cmd.exe /c usbipd attach --wsl --busid=$BUSID
+
+#Give some time the device to be recognized
+sleep 2 
+
+# Step 3: Reboot Pico into BOOTSEL mode
+echo -e " $CYAN Rebooting Pico into BOOTSEL mode... "
+if ! sudo picotool reboot -f -u; then
+   echo -e " $RED Failed to reboot into BOOTSEL mode. Retrying... "
+   sleep 1
+   sudo picotool reboot -f -u || { echo -e "{$RED}Second attempt failed. Exiting. "; exit 1; }
 else 
-	echo "build dir does not exist."
-	mkdir  "$build_dir"
-	echo "build from scratch"
-	cd "$build_dir"
-	cmake .. || goto error
-fi	
-cd app
-make || goto error
+  echo -e "$CYAN BOOTSEL successful "
+fi
 
-echo "Firmware was built!"
-read -p  "Uploading to rp2350..."
-picotool reboot -f -u || goto error
-sleep 2s
-picotool load main.uf2 || goto error
-sleep 1s
-picotool reboot || goto error
+#wait 
+sleep 2
 
-echo "main.uf2 was uploaded to rp2350!"
+#Step 4: Attach USB device (BOOTSEL mode)
+#List USB devices
+cmd.exe /c usbipd list > log2.txt
+#Look for 2e8a:0009 VID:PID and chain the corresponding line to fragment using awk and return the first field BUSID
+BUSID=$(grep '2e8a:000f' log2.txt | awk '{print $1}')
+echo "Busid:"
+echo $BUSID
 
-cd "$current_dir"
+#Attach to the BUSID
+cmd.exe /c usbipd attach --wsl --busid=1-4
 
-:error  
-echo "An error occurred during the build or upload process."
-cd "$current_dir"
-exit 1
+#waits are very important
+sleep 2 
+
+#Step 5: Load firmware
+#Navigate to app folder
+echo -e "$CYAN Load firmware..."
+sudo picotool load main.uf2
+
+echo -e "$PURPLE Detach device"
+sudo picotool reboot
+cmd.exe /c usbipd detach --busid=$BUSID
